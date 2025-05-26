0830ff142a0dc7b198eb869c959bbac3cdfceca4
Simon Laptop <s_frost@me.com>
2025-05-26 16:30:23 +0200

    adjusted stock warning logic

--- stock-warning.php
+++ stock-warning.php
@@ -2,7 +2,7 @@
 // Disaply message if a product is in stock and quantity is less than 10
 if (is_product()) {
     global $product;
-    if ($product->is_in_stock() && $product->managing_stock() && $product->get_stock_quantity() < 3) {
+    if ($product->is_in_stock() && $product->managing_stock() && $product->get_stock_quantity() > 0 && $product->get_stock_quantity() < 3) {
         echo '<p class="stock-warning">🔥<span>Selling out FAST</span>🔥 Grab yours before they\'re gone!</p>';
     }
 }
